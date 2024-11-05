745d989a0a2f26e35f911eeead93e522ec95c892
Naveen Dasanayaka <>
2024-11-05 23:18:25 +0000

    basic setup

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {
+
+  searchString: string = ''
+  reverseString: string = ''
+  uniqueChar: string | null = null;
+
+  search(){
+    
+  }
+
+}
