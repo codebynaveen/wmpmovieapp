2abf1d157c61b652f81d822966fdaa421ce0faf2
dileepa <dileepa9646@gmail.com>
2024-11-09 12:46:11 +0530

    enhancement: fixed the router link active class issue and update the search page.

--- src/app/components/main-navigation/main-navigation.component.ts
+++ src/app/components/main-navigation/main-navigation.component.ts
@@ -1,15 +1,20 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {Router} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-main-navigation',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './main-navigation.component.html',
   styleUrl: './main-navigation.component.scss'
 })
 export class MainNavigationComponent {
+
+  constructor(public router: Router) {}
 
 }
--- src/app/pages/episode-search/episode-search.component.ts
+++ src/app/pages/episode-search/episode-search.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { EpisodeCardComponent } from '../../components/episode-card/episode-card.component';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgOptimizedImage } from '@angular/common';
 import { TopEpisodesListComponent } from '../../components/top-episodes-list/top-episodes-list.component';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { EpisodesService } from '../../core/services/episodes.service';
-import {SearchBoxComponent} from '../../components/search-box/search-box.component';
+import { SearchBoxComponent } from '../../components/search-box/search-box.component';
 
 @Component({
   selector: 'app-episode-search',
@@ -15,7 +15,8 @@ import {SearchBoxComponent} from '../../components/search-box/search-box.compone
     NgForOf,
     TopEpisodesListComponent,
     FaIconComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    NgOptimizedImage
   ],
   templateUrl: './episode-search.component.html',
   styleUrl: './episode-search.component.scss'
